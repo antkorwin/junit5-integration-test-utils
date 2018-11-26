33b55816dc2804e86797c29c5c7b3dc1945cc0c7
Anatoliy Korovin <antkorwin@gmail.com>
2018-11-26 17:37:41 +1000

    add javadoc

--- src/main/java/com/antkorwin/junit5integrationtestutils/test/extensions/benchmark/MeasureUnit.java
+++ src/main/java/com/antkorwin/junit5integrationtestutils/test/extensions/benchmark/MeasureUnit.java
@@ -11,6 +11,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 /**
  * Created on 17.08.2018.
  *
+ * This annotation provides an ability to set a unit of measure,
+ * which will use in the ProfilerExtension and BenchmarkExtension.
+ *
+ * If you need to profile a very fast process then you can set
+ * a MeasureUnit to NANOSECONDS, after that the ProfilerExtension
+ * will use the System.nanoTime() to measure duration.
+ *
+ * By default, ProfilerExtension use a MILLISECONDS setting
+ *
  * @author Korovin Anatoliy
  */
 @Target(ElementType.TYPE)
